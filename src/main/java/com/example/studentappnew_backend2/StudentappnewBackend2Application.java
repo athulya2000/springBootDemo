7af8eeb1aa8c17cfecce4dabd4e6382d3d57e05a
athulya2000 <athulyav00@gmail.com>
2022-12-05 14:30:25 +0530

    initial version

---
+++ src/main/java/com/example/studentappnew_backend2/StudentappnewBackend2Application.java
@@ -0,0 +1,13 @@
+package com.example.studentappnew_backend2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentappnewBackend2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentappnewBackend2Application.class, args);
+	}
+
+}
