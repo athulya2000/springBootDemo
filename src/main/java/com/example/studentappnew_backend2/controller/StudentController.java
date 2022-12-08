3a249346482a7457e4460d9caafce18f40b19196
athulya2000 <athulyav00@gmail.com>
2022-12-08 15:44:25 +0530

    view

--- src/main/java/com/example/studentappnew_backend2/controller/StudentController.java
+++ src/main/java/com/example/studentappnew_backend2/controller/StudentController.java
@@ -5,20 +5,31 @@ import com.example.studentappnew_backend2.model.Students;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 @RestController
 public class StudentController {
     @Autowired
     private StudentsDao dao;
     @CrossOrigin(origins = "*")
    @PostMapping(path="/add",consumes = "application/json",produces = "application/json")
-    public String AddStudents(@RequestBody Students s){
+    public Map<String,String> AddStudents(@RequestBody Students s){
        System.out.println(s.getName().toString());
        System.out.println(s.getRollNo().toString());
        System.out.println(s.getAdmissionNumber().toString());
        System.out.println(s.getCollege().toString());
        dao.save(s);
-        return "Welcome to my website";
+        HashMap<String,String> map=new HashMap<>();
+        map.put("status","success");
+        return map;
 
+    }
+    @CrossOrigin(origins = "*")
+    @GetMapping("/view")
+    public List<Students> View(){
+        return(List<Students>) dao.findAll();
     }
 
 
