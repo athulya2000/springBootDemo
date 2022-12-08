b33f7c79204daf09536377f21bfe67cf7de62a5a
athulya2000 <athulyav00@gmail.com>
2022-12-08 16:14:48 +0530

    search

--- src/main/java/com/example/studentappnew_backend2/dao/StudentsDao.java
+++ src/main/java/com/example/studentappnew_backend2/dao/StudentsDao.java
@@ -1,7 +1,13 @@
 package com.example.studentappnew_backend2.dao;
 
 import com.example.studentappnew_backend2.model.Students;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface StudentsDao extends CrudRepository<Students,Integer> {
+    @Query(value="SELECT `id`, `admission_number`, `college`, `name`, `roll_no` FROM `students` WHERE `admission_number`= :admissionNumber",nativeQuery = true)
+    List<Students> SearchStudent(@Param("admissionNumber") String admissionNumber);
 }
